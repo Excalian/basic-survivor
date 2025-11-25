18006ae1c142c08db74790a9829c7f8bdc4fed85
Marcellino Abdelmalak <me@excalian.com>
2025-11-24 17:21:00 -0800

    Added wizard enemy at 30 seconds in with a 30 percent chance

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,14 +3,18 @@ extends Node
 var spawn_radius: float
 
 @export var basic_enemy_scene: PackedScene
+@export var wizard_enemy_scene: PackedScene
 @export var arena_time_manager: Node
 
 @onready var timer = $Timer
 
 var base_spawn_time: float
+var enemy_table = WeightedTable.new()
 
 
 func _ready() -> void:
+	enemy_table.add_item(basic_enemy_scene, 10)
+	
 	base_spawn_time = timer.wait_time
 	var viewport_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 	var viewport_height = ProjectSettings.get_setting("display/window/size/viewport_height")
@@ -54,7 +58,8 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
-	var enemy: Node2D = basic_enemy_scene.instantiate()
+	var enemy_scene = enemy_table.pick_item()
+	var enemy: Node2D = enemy_scene.instantiate()
 	
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	if entities_layer == null:
@@ -68,3 +73,6 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	var time_off = (.1 / 12) * arena_difficulty
 	time_off = min(time_off, .7)
 	timer.wait_time = base_spawn_time - time_off
+	
+	if arena_difficulty == 6:
+		enemy_table.add_item(wizard_enemy_scene, 20)
